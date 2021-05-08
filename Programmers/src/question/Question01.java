2daa12e676963c56d20230c2567a2fac4bb0ed61
heesun <yh82076@naver.com>
2021-05-08 18:32:09 +0900

    Programmers_ver 1.1

--- Programmers/src/question/Question01.java
+++ Programmers/src/question/Question01.java
@@ -1,9 +1,16 @@
 package question;
 
+import java.util.Arrays;
+
 public class Question01 {
 	
 	public static void main(String[] args) {
 		
+		System.out.println("answer: "+solution());
+		
+	}
+
+	public static String solution() {
 		//ex1
 		String[] participant = {"leo", "kiki", "eden"};
 		String[] completion = {"eden", "kiki"};
@@ -14,38 +21,34 @@ public class Question01 {
 //		String[] participant = {"mislav", "stanko", "mislav", "ana"};
 //		String[] completion = {"stanko", "ana", "mislav"};
 		String answer = "";
-		String over = "";
-		
+
 		/*
-       	a. participant 배열에 중복원소 찾아내기.
-            - participant 배열[1]~[length] 와 
-		    - participant 배열[0]~[length] 을 차례대로 비교. 		    
-	    b. completion [0] ~ [length] 를 participant와 비교 
-	    c. participant에 존재하고, 중복원소에 포함되지 않으면 pass
-            participant 모든 원소 비교후 존재하지 않으면 answer 저장 
-        */
+		 	a. 각각 배열 정렬 
+		 	b. 정렬된 배열을 차례대로 비교  (participant를 completion과 비교)
+		 	c-1. 동명이인이 없는 경우 > 모든 원소 비교 후에도 다른 값이 나오지 않음 > participant 마지막 인덱스 값이 미완주자
+		 	c-2. 동명이인이 있는 경우 > participant에는 있고, completion에는 없는 경우 > 값이 다른 인덱스의 participant 값이 미완주자
+		  
+		*/
 		
-		//a. 
-        for(int i=0; i<participant.length; i++){
-            for(int j=i+1; j<participant.length; j++){
-                if(participant[i].equals(participant[j])){
-                    over = participant[i];
-                } 
-            }
-        }
-        
-        //b.
-        for(int n=0; n<completion.length; n++){
-            for(int m=0; m<participant.length; m++){
-                //c.
-                if(!(completion[n].equals(participant[m]) && !completion[n].equals(over))) {
-                    answer = participant[m];
-                }
-            }
-        }
+		Arrays.sort(participant);
+		Arrays.sort(completion);
 		
-		System.out.println(answer);
+//		System.out.println("participant "+Arrays.toString(participant));
+//		System.out.println("completion "+Arrays.toString(completion));
 		
+		
+		for(int i=0; i<completion.length; i++) {
+			if(!participant[i].equals(completion[i])) {
+				answer = participant[i];
+				return answer;
+			}
+		}
+		
+		//for문 다 돌아도 다른 값이 안나왔을 경우
+		answer = participant[participant.length-1];
+		return answer;
+	
+
 	}
 
 }
