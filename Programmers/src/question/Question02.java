c80962765f45434a0d0860eddb1f8b0ad4b2d666
heesun <yh82076@naver.com>
2021-08-13 00:22:10 +0900

    Programmers_ver 1.5

--- Programmers/src/question/Question02.java
+++ Programmers/src/question/Question02.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 public class Question02 {
 	 public int[] solution(int[] array, int[][] commands) {
-	        int[] answer = {};
+	        int[] answer =  new int[commands.length];
 	        //복사할 배열 
-	        int[] tmp = {};
+//	        int[] tmp = {};
 	        // answer[0] = commands[0][0];
 	        // System.out.println(answer.length);
 	        
@@ -14,17 +14,17 @@ public class Question02 {
 	        // answer[1] = commands[0][1];
 	        // answer[2] = commands[0][2];
 	        
-	        for(int i=0; i<3; i++){
+	        for(int i=0; i<commands.length; i++){
 	            // a. i번째 ~ j번째로 배열 잘라서 tmp에 저장 
-	            tmp = Arrays.copyOfRange(array,commands[i][0],commands[i][1]);
+	            int[] tmp = Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1]);
 	            
-	            System.out.println(tmp[i]);
+//	            System.out.println(tmp[i]);
+	            Arrays.sort(tmp);
 	            
 	            //b. 그중 k번째 원소 꺼내서 return 할 배열변수에 누적 
-	            answer[i] = tmp[commands[i][2]];
+	            answer[i] = tmp[commands[i][2]-1];
 	        
 	        }
-
 	        
 	        return answer;
 	    }
