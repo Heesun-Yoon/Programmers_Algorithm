1b1a3196d17d98a2e5be64215a8b0a55d2618d00
heesun <yh82076@naver.com>
2021-08-12 22:58:24 +0900

    Programmers_ver 1.4

--- Programmers/src/question/Question02.java
+++ Programmers/src/question/Question02.java
@@ -1,5 +1,32 @@
 package question;
 
+import java.util.Arrays;
+
 public class Question02 {
+	 public int[] solution(int[] array, int[][] commands) {
+	        int[] answer = {};
+	        //복사할 배열 
+	        int[] tmp = {};
+	        // answer[0] = commands[0][0];
+	        // System.out.println(answer.length);
+	        
+	        //  answer[0] = commands[0][0];
+	        // answer[1] = commands[0][1];
+	        // answer[2] = commands[0][2];
+	        
+	        for(int i=0; i<3; i++){
+	            // a. i번째 ~ j번째로 배열 잘라서 tmp에 저장 
+	            tmp = Arrays.copyOfRange(array,commands[i][0],commands[i][1]);
+	            
+	            System.out.println(tmp[i]);
+	            
+	            //b. 그중 k번째 원소 꺼내서 return 할 배열변수에 누적 
+	            answer[i] = tmp[commands[i][2]];
+	        
+	        }
+
+	        
+	        return answer;
+	    }
 
 }
