962d275cc16fbc57273d7498a7c98ad848cebb8c
heesun <yh82076@naver.com>
2021-10-24 23:41:40 +0900

    javascript_study_ver 1.9

--- JavaSciprt_Study/javascript_1.js
+++ JavaSciprt_Study/javascript_1.js
@@ -1,1 +1,41 @@
 //2021.10.24 javascript study 
+
+ /*
+     chapter 8. 자료형
+ */
+
+//숫자형
+var x = 5;
+
+//문자형
+var y = 'five';
+
+//불린형
+var isTrue = true;
+
+//null
+var empty = null;
+
+//undefined
+var nothing
+
+//Symbol
+var sym = Symbol('me');
+
+//객체(object)
+var item = {
+    price: 5000,
+    count: 10
+};
+
+//배열(Array)
+var fruits = ['apple','orange','kiwi'];
+
+//함수(function)
+var addFruit = function (fruit) {
+    fruits.push(fruit);
+}
+//함수 호출
+addFruit('watermelon');
+
+console.log(fruits);
