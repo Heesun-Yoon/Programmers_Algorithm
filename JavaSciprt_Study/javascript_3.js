75ffe0d604fb45957d44a213d09e592ce06b5122
heesun <yh82076@naver.com>
2021-10-27 20:17:50 +0900

    javascript_study_ver 2.1

---
+++ JavaSciprt_Study/javascript_3.js
@@ -0,0 +1,13 @@
+//2021.10.27 javascript study 
+
+ /*
+     chapter 24. 삼항 연산자
+ */
+
+
+// 조건문 ? 표현문1 : 표현문2 
+// 조건문이 참 일때 표현문1 실행, 거짓일 때 표현문2 실행
+
+var condition = 5 > 10;
+condition ? console.log('참') : console.log('거짓');
+
