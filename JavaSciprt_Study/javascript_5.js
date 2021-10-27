acc38ed4b1448a111efeb4a8cf9fa19158d31272
heesun <yh82076@naver.com>
2021-10-27 22:55:57 +0900

    javascript_study_ver 2.2

--- JavaSciprt_Study/javascript_5.js
+++ JavaSciprt_Study/javascript_5.js
@@ -2,4 +2,107 @@
      chapter 39. 스코프 
  */
 
+//스코프는 유효 범위로써 변수와 매개변수가 어디까지 유효한지 나타냄 
+//ex) 변수 a가 어디를 참조할지, 실제 값이 무엇인지 찾을 때 사용,,
+//기본적으로 전역과 함수 단위로 스코프 생성 (전역에 선언한 변수는 어디에서나 접근 가능)
+
+//1.
+//전역 스코프 
+var a = 10;
+console.log(a);
+
+function print() {
+    //지역 스코프
+    var b = 20;
+    if(true) {
+        //지역 스코프
+        var c = 30;
+    }
+    //print 함수 안에서는 모두 c 접근 가능 
+    console.log(c)
+}
+
+print();
+//ReferenceError: b is not defined
+//b는 해당 함수 내에서만 접근 가능 
+// console.log(b);
+
+
+
      
+//2.
+var a = "global";
+
+function print1() {
+    console.log(a);
+}
+
+function print2() {
+    var a = "local";
+    print1();
+}
+
+//둘다 global
+//자바스크립트는 렉시컬 스코프이기 때문에 코드 작성하는 시점에 확정. 
+// > print1이 작성될 때는 이미 전역 a를 참조 하고 있기 때문에 global 출력
+print1();
+print2();
+
+
+
+
+
+
+ /*
+     chapter 40. 함수 호이스팅 
+ */
+
+//호이스팅 : 자바스크립트에서는 함수를 선언하기 전에 호출 가능 
+// > 직역하면 끌어 올리기 인데, 함수가 실제 호출하기 이전으로 끌어 올라간 것처럼 동작 
+
+
+//함수
+hello();
+function hello() {
+    console.log('안녕');
+}
+
+
+//표현식 
+//Uncaught TypeError: hello2 is not a function
+//hello2 이름으로 선언된 변수는 호이스팅이 이루어짐. 여기에 undefined 할당됨 
+// hello2();
+var hello2 = function() {
+    console.log('안녕2');
+}
+
+
+
+
+ /*
+     chapter 41. let
+ */
+
+if(true) {
+    //var로 정의한 변수는 함수 단위의 유효 범위 가짐. 블록 밖에서도 접근 가능
+    var f = 'global';
+    //let으로 정의한 변수는 블록 단위의 유효 범위 가짐. 블록 밖에서 접근 불가
+    let b = 'local';
+}
+
+console.log(f);
+//Uncaught ReferenceError: b is not defined
+// console.log(b);
+
+
+let value = "바깥";
+if(ture) {
+    //Uncaught ReferenceError: ture is not defined
+    //블록 밖에서 선언된 let 변수는 접근 X
+    console.log(value);
+    let value = "안";
+}
+
+
+
+
---
+++ JavaSciprt_Study/javascript_6.js
@@ -0,0 +1,78 @@
+
+/*
+     chapter 42. const
+*/
+
+//let과 마찬가지로 블록 단위로 스코프 정의 가능. 대신 선언 시 값을 할당해야 하고 이후에 재할당 불가 
+
+const URL = 'http://js.com';
+//ypeError: Assignment to constant variable.
+//const로 선언된 변수는 재할당 불가
+// URL = 'http://js.com';
+
+if(true) {
+    const URL2 = 'http://js.com';
+}
+
+//URL2 변수는 블록 내에서 선언되었기 때문에 밖에서 접근 불가
+// console.log(URL2);
+
+
+
+
+/*
+     chapter 43. 스코프 체인 
+*/
+
+//스코프 체인 : 문자 그대로 스코프가 연결되어 있음 
+
+var person = 'harin';
+
+function print() {
+    var person2 = 'jay';
+
+    function innerPrint() {
+        console.log(person);
+        console.log(person2);
+    }
+    innerPrint();
+
+    console.log('print finished');
+}
+
+print();
+console.log('finished');
+
+
+
+
+/*
+     chapter 44. 클로저
+*/
+
+//*렉시컬 환경(Lexical Environment): 식별자와 값을 관리하고, 외부 상위 스코프에 대한 참조를 기록하는 데 사용하는 자료구조. 환경 레코드와 외부 렉시컬 환경 참조에 대한 정보로 구성되어 있다.
+//클로저 : 함수가 정의될 때의 렉시컬 환경을 기억하는 함수
+
+function create() {
+    let count = 0;
+    return {
+        increase: function() {
+            count++;
+        },
+        getCount: function() {
+            return count;
+        }
+    };
+}
+
+//create 함수에는 객체를 반환 (increase, getCount)
+//counter1 은 create 함수에서 반환된 2개의 객체 가지고 있음 
+const counter1 = create();
+const counter2 = create();
+
+counter1.increase();
+counter1.increase();
+console.log('counter 1의 값 : ' + counter1.getCount());
+
+counter2.increase();
+console.log('counter 2의 값 : ' + counter2.getCount());
